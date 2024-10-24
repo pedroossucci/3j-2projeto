28014c0bd563a70eba7864647fa3876ddadc3117
pipoket <110671067+pedroossucci@users.noreply.github.com>
2024-10-24 07:34:07 -0300

    Update quantidadeusuarios.js

--- graficos/quantidadeusuarios.js
+++ graficos/quantidadeusuarios.js
@@ -1,28 +1,30 @@
 import { getCSS } from "./comum.js";
 
-async function quantidadeusuarios(){
+async function quantidadeDeUsuarios(){
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const resultado = await fetch(url);
     const dados = await resultado.json();
-    const nomeredes = Object.keys(dados);
-    const quantidadeusuarios = Object.values(dados);
-
+    const nomeDasRedes = Object.keys(dados);
+    const quantidadeDeUsuarios = Object.values(dados);
 
     const infos = [
         {
-            x: nomeredes,
-            y: quantidadeusuarios,
-            type: 'bar', 
+            x: nomeDasRedes,
+            y: quantidadeDeUsuarios,
+            type: 'bar',
             marker: {
                 color: getCSS('--cor-primaria')
             }
         }
     ]
-
+    const layout = {
+        plot_bgcolor: getCSS('--cor-de-fundo'),
+        paper_bgcolor: getCSS('--cor-de-fundo')
+    }
     const grafico = document.createElement('div');
-    grafico.className = 'grafico'
-    document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico,infos)
-} 
+    grafico.className = 'grafico';
+    document.getElementById('graficos-container').appendChild(grafico);
+    Plotly.newPlot(grafico,infos,layout)
+}
 
-quantidadeusuarios()
+quantidadeDeUsuarios()

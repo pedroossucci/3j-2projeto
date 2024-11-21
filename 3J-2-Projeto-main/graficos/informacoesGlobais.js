ee23fe61e147f98e6a5c6e3c00dcbaa84cb6d415
pipoket <110671067+pedroossucci@users.noreply.github.com>
2024-11-21 08:14:40 -0300

    Add files via upload

---
+++ 3J-2-Projeto-main/graficos/informacoesGlobais.js
@@ -0,0 +1,17 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json';
+
+async function visualizarDadosGlobais(){
+    const res = await fetch(url);
+    const secao = document.querySelector('.graficos-container');
+    const dados= await res.json();
+    const totalPessoasMundo = dados.total_pessoas_mundo
+    const totalPessoasConectadas = dados.total_pessoas_conectadas
+    const tempoMedio = dados.tempo_medio
+    console.log (dados);
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('grafico-texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${totalPessoasMundo}</span> de habitantes e destes <span>${totalPessoasConectadas}</span> estão conectadas a alguma rede social e ficam em média <span>${tempoMedio}</span> conectadas.`
+    secao.appendChild(paragrafo)
+}
+
+visualizarDadosGlobais();

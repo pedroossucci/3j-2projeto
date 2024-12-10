b8320c16f3c467dde594d7083ab0abbf0d47437a
pipoket <110671067+pedroossucci@users.noreply.github.com>
2024-12-10 18:48:36 -0300

    Update redesFavoritas.js

--- graficos/redesFavoritas.js
+++ graficos/redesFavoritas.js
@@ -1,29 +1,65 @@
-async function redesFavoritas(){
+import { getCSS } from "./comum.js";  
 
-    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json';
+async function redesFavoritasMundo() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json'; 
+    const res = await fetch(url); 
+    const dados = await res.json();  
+    console.log(dados);  
     
-    const res = await fetch(url);
-    
-    const dados = await res.json();
-    
+ 
     const redes = Object.keys(dados);
-    
     const valores = Object.values(dados);
     
-    const infos = [
+    const data = [
+        {
+            values: valores,
+            labels: redes,
+            type: 'pie',  
+            textinfo: 'label+percent'  
+        }
+    ];
+
+    const tickfont = {
+        family: 'Arial',
+        size: 12,
+        color: getCSS('--cor-secundaria')  
+    };
+
     
-    {
+    const layout = {
+        plot_bgcolor: getCSS('--cor-de-fundo'),  
+        paper_bgcolor: getCSS('--cor-de-fundo'),  
+        height: 700,  
+        title: {
+            text: 'Redes sociais com mais usuários no mundo',  
+            x: 0,  
+            font: {
+                color: getCSS('--cor-primaria'), 
+                size: 16 
+            }
+        },
+        xaxis: {
+            tickfont: tickfont,  
+            title: {
+                text: 'Nome das redes sociais',  
+                font: {
+                    color: getCSS('--cor-secundaria') 
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickfont,  
+            title: {
+                text: 'Milhões de usuários ativos',  
+                font: {
+                    color: getCSS('--cor-secundaria')
+                }
+            }
+        }
+    };
+
     
-    values: valores,
-    
-    labels: redes,
-    
-    type: 'pie',
-    
-    textinfo: 'label+percent'
-    
-    }
- ];
+    criarGrafico(data, layout);
 }
-    
-   
+
+redesFavoritasMundo();

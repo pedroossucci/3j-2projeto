f8cd42c151e7f57c3946d9a5d40ec49d8a848e7f
pipoket <110671067+pedroossucci@users.noreply.github.com>
2024-12-12 07:42:24 -0300

    Add files via upload

--- graficos/comum.js
+++ graficos/comum.js
@@ -8,18 +8,17 @@ const tickfont = {
     family: getCSS('--fonte')
 }
 
-export {getCSS, tickfont}
 
-function criarGrafico(data, layout){
+function criarGrafico(data, layout) {
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
-    document.getElementById('graficos-containe') .appendChild(grafico)
+    document.getElementById('graficos-containe').appendChild(grafico)
     const config = {
         responsive: true,
         displayModeBar: false
-        }
-    Plotly.newPlot(grafico, data, layout, config)
     }
-    
-    export {getCSS, tickConfig, criarGrafico}
+    Plotly.newPlot(grafico, data, layout, config)
+}
+
+export { getCSS, tickConfig, criarGrafico }
 
